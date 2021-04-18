20c15282a2d5067fb2aeaf34a067cf805dd19f61
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-18 09:08:59 -0700

    add AllInfo class

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,53 @@
 //Business Logic
-let sampleInfo = {"Sam":{"dayChoices":["Yes", "No", "Yes", "", ""], "best":3, "min":3, "max":3}, 
-"Jack":{"dayChoices":["No", "No", "", "", ""], "best":2, "min":2, "max":2},
-"Jill":{"dayChoices":["Yes", "", "Yes", "", "Yes"], "best":2, "min":2, "max":2}};
+class AllInfo{
+  constructor(){
+    this.info={};
+    this.currentID=0;
+  }
 
-function ChildInfo(name, mon, tue, wed, thu, fri, best, min, max){
+  addChildInfo(childInfo){
+    this.currentID ++;
+    this.info[this.currentID]=childInfo;
+  }
+
+  getDaysById(id){
+    let dayCount=0;
+    this.info[id]["dayChoices"].forEach(function(day){
+      if (day==="Yes") {
+        dayCount ++;
+      }
+    });
+    return dayCount;
+  }
+
+  getDaysByWeekDay(weekday) {
+    let dayCount=0;
+    for(let id in this.info){
+      if(this.info[id]["dayChoices"][weekday]==="Yes"){
+        dayCount++;
+      }
+    };
+    return dayCount;
+  }
+}
+
+class ChildInfo{
+  constructor(name, mon, tue, wed, thu, fri, best, min, max){
     this.name=name;
     this.dayChoices=[mon, tue, wed, thu, fri];
     this.best=best;
     this.min=min;
     this.max=max;
-}
-
-function getDaysByName(nameInDictionary) {
-  let dayCount=0;
-  nameInDictionary["dayChoices"].forEach(function(day){
-    if (day==="Yes") {
-      dayCount ++;
-    }
-  });
-  return dayCount;
-}
-
-function getDaysByWeekDay(dictionary, weekday) {
-  let dayCount=0;
-  for(let person in dictionary){
-    if(dictionary[person]["dayChoices"][weekday]==="Yes"){
-      dayCount++;
-    }
-  };
-  return dayCount;
+  }
 }
 
 
 //TESTS
-let result=getDaysByName(sampleInfo["Sam"]);
-console.log(2)
-console.log(result)
+let sampleInfo = {1:{name: "Sam", "dayChoices":["Yes", "No", "Yes", "", ""], "best":3, "min":3, "max":3}, 
+2:{name:"Jack", "dayChoices":["No", "No", "", "", ""], "best":2, "min":2, "max":2},
+3:{name:"Jill", "dayChoices":["Yes", "", "Yes", "", "Yes"], "best":2, "min":2, "max":2}};
 
-result=getDaysByWeekDay(sampleInfo, 4);
-console.log(1)
-console.log(result);
-
-let lily=new ChildInfo("Lily", "", "", "", "", "Yes", 3, 2, 3);
-console.log("Lily " + "Yes " + 3);
-console.log(lily.name, lily.dayChoices[4], lily.best);
+let allInfo = new AllInfo();
+let lily=new ChildInfo("Lily", "", "Yes", "", "", "Yes", 3, 2, 3);
+allInfo.addChildInfo(lily);
+console.log(allInfo.getDaysById(1));
